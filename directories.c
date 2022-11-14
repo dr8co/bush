4344e56129f26adacfc2c1baedeb2f0fb15e9bcb
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2022-11-14 19:46:17 +0300

    Fix bugs in change directory

--- directories.c
+++ directories.c
@@ -11,12 +11,21 @@ char cwd[1024];
 
 void change_directory() {
     char *home_dir = getenv("HOME");
+    char *old_dir = getenv("OLDPWD");
 
     if (home_dir == NULL) {
         home_dir = "/home";
     }
     if ((args[1] == NULL) || (strcmp(args[1], "~") == 0) || (strcmp(args[1], "~/") == 0))
         chdir(home_dir);
+    else if (strcmp(args[1], "-") == 0) {
+        if (old_dir != NULL) {
+            chdir(old_dir);
+        }
+        else{
+            printf("%s: %i: cd: OLDPWD not set\n", absolute_shell_name, cmd_count);
+        }
+    }
     else if (chdir(args[1]) < 0)
         printf("%s: %i: cd: can\'t cd to %s\n", absolute_shell_name, cmd_count, args[1]);
 
@@ -36,11 +45,10 @@ char *abs_name() {
     char *tmp2 = malloc(sizeof(char) * 1024);
 
     strcpy(tmp2, current_directory);
-    strcat(tmp2, "/");
 
-    if (shell_name[0] == '.' && (shell_name[1] == '\\' || shell_name[1] == '/')) {
-        while (shell_name[i + 2] != '\0') {
-            *(tmp + i) = shell_name[i + 2];
+    if (shell_name[0] == '.' && shell_name[1] == '/') {
+        while (shell_name[i + 1] != '\0') {
+            *(tmp + i) = shell_name[i + 1];
             i++;
         }
         tmp[i] = '\0';
