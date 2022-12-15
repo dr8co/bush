3d8e2122fe99be23fe3570fc90bb27e5e54ca53f
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2022-12-15 21:51:09 +0300

    Update scripts

--- compile.sh
+++ compile.sh
@@ -30,11 +30,13 @@ if [ -x "$(command -v clang)" ]; then
   CLANG_VERSION=$(clang --version | grep -n 1 | awk '{split($4, ver, "."); print ver[1]}')
   set_std_flag
   clang -Wall -Werror -pedantic "$C_STD" ./src/*.c -o bush
+  echo "Compiled with Clang $CLANG_VERSION with C standard $C_STD"
 else
   if [ -x "$(command -v gcc)" ]; then
     GCC_VERSION=$(gcc --version | grep gcc | awk '{split($3, ver, "."); print ver[1]}')
     set_std_flag
     gcc -Wall -Werror -pedantic "$C_STD" ./src/*.c -o bush
+    echo -e "Compiled with GCC $GCC_VERSION with C standard $C_STD"
   else
     echo "clang and gcc compilers not found in your system. Please compile manually."
     exit "$EXIT_CODE"
