7e83ed66a6f7fbb7dcb42e37888c34df41a2478b
Ian Duncan <76043277+dr8co@users.noreply.github.com>
2022-12-15 23:14:33 +0300

    Update scripts

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 # Run from this directory
 cd "${0%/*}" || exit 1
 
+# The script must be run with superuser powers.
 if [ "$(id -u)" != "0" ]; then
   echo "Sorry, you are not root."
   exit 1
@@ -24,6 +25,7 @@ echo -e "Compiling..\n"
 
 CODE=$?
 
+# Exit if compilation failed.
 if [ $CODE == $EXIT_CODE ]; then
   echo -e "Compilation Failed. Please resolve the issue and try again."
   exit 1
--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Run from this directory.
+cd "${0%/*}" || exit 1
+
+# sudo powers required.
 if [ "$(id -u)" != "0" ]; then
   echo "Sorry, you are not root."
   exit 1
@@ -13,30 +17,28 @@ MAN_PATH="/usr/local/share/man/man1"
 
 echo -e "Uninstalling The Burning Bush..\n"
 
+echo "Cleaning man page.."
 if [ -f "$MAN_PATH/burning-bush.1" ]; then
   rm -f "$MAN_PATH/burning-bush.1"
 fi
-
 
 if [ -f "$MAN_PATH/burning-bush.1.gz" ]; then
   rm -f "$MAN_PATH/burning-bush.1.gz"
 fi
 
-
+echo "Cleaning installed bush binary.."
 if [ -f "$BIN_PATH/$BUSH" ]; then
   rm -f "$BIN_PATH/$BUSH"
 fi
-
 
 if [ -d "$APP_PATH" ]; then
   rm -rf "$APP_PATH"
 fi
 
-
+echo "Cleaning compiled binary.."
 if [ -f "$BUSH" ]; then
   rm -f "$BUSH"
 fi
-
 
 echo -e "Updating man database..\n"
 
